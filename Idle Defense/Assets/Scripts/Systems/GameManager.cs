08b120fa590d0ec5de36340e27b63aa3aafbf856
Dartiro <beorgames@gmail.com>
2025-03-31 09:46:06 +0200

    Changed to new input system
    also added spacebar as input

--- Idle Defense/Assets/Scripts/Systems/GameManager.cs
+++ Idle Defense/Assets/Scripts/Systems/GameManager.cs
@@ -3,6 +3,7 @@ using Assets.Scripts.WaveSystem;
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace Assets.Scripts.Systems
 {
@@ -12,11 +13,15 @@ namespace Assets.Scripts.Systems
         public float spdBonus { get; private set; }
 
         private const float maxSpdBonus = 200f;
-        private const float increaseRate = maxSpdBonus / 10f; // Increase to 200 in 10 seconds
-        private const float decreaseRate = maxSpdBonus / 30f; // Decrease to 0 in 30 seconds
-        private float _spdBonusDecreaseTimer = 0f;
-        private bool _spdBonusDecreasing;
-        private const float _spdBonusDecreaseDelay = 2f;
+        private const float holdIncreaseRate = 50f;
+        private const float initialBoost = 5f;
+
+        private bool isHolding = false;
+        private float decreaseDelay = 1f;
+        private float decreaseTimer = 0f;
+
+        [SerializeField] private PlayerInput _playerInput;
+        private InputAction _clickAction;
 
         public static GameManager Instance { get; private set; }
 
@@ -26,6 +31,11 @@ namespace Assets.Scripts.Systems
                 Instance = this;
             else
                 Destroy(gameObject);
+                        
+            _clickAction = _playerInput.actions["Click"];
+
+            _clickAction.performed += OnClickStarted;
+            _clickAction.canceled += OnClickReleased;
         }
 
         private void Start()
@@ -33,11 +43,53 @@ namespace Assets.Scripts.Systems
             EnemySpawner.Instance.OnWaveCompleted += OnWaveCompleted;
         }
 
+        private void OnDestroy()
+        {
+            _clickAction.performed -= OnClickStarted;
+            _clickAction.canceled -= OnClickReleased;
+        }
+
+        private void OnClickStarted(InputAction.CallbackContext ctx)
+        {
+            spdBonus += initialBoost;
+            isHolding = true;
+            decreaseTimer = 0f;
+        }
+
+        private void OnClickReleased(InputAction.CallbackContext ctx)
+        {
+            isHolding = false;
+            decreaseTimer = 0f;
+        }
+
+
         private void Update()
         {
-            if (!EventSystem.current.IsPointerOverGameObject())
-                AdjustSpdBonus();
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            if (isHolding)
+            {
+                spdBonus += holdIncreaseRate * Time.deltaTime;
+                decreaseTimer = 0f;
+            }
+            else
+            {
+                if (decreaseTimer >= decreaseDelay)
+                {
+                    spdBonus -= holdIncreaseRate * 0.5f * Time.deltaTime;
+                }
+                else
+                {
+                    decreaseTimer += Time.deltaTime;
+                }
+            }
+
+            spdBonus = Mathf.Clamp(spdBonus, 0f, maxSpdBonus);
+            UIManager.Instance.UpdateSpdBonus(spdBonus);
+
         }
+
 
         private void OnWaveCompleted(object sender, EventArgs e)
         {
@@ -50,50 +102,6 @@ namespace Assets.Scripts.Systems
             dmgBonus = 0;
         }
 
-        private void AdjustSpdBonus()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                if (_spdBonusDecreasing)
-                {
-                    _spdBonusDecreasing = false;
-                    _spdBonusDecreaseTimer = 0f;
-                }
-
-                IncreaseSpdBonus();
-            }
-            else
-            {
-                if (!_spdBonusDecreasing)
-                {
-                    CountdownSpdBonusDecrease();
-                }
-                else
-                {
-                    DecreaseSpdBonus();
-                }
-            }
-
-            spdBonus = Mathf.Clamp(spdBonus, 0, maxSpdBonus);
-
-            UIManager.Instance.UpdateSpdBonus(spdBonus);
-        }
-
-        private void IncreaseSpdBonus()
-        {
-            spdBonus += increaseRate * Time.deltaTime;
-        }
-
-        private void DecreaseSpdBonus()
-        {
-            spdBonus -= decreaseRate * Time.deltaTime;
-        }
-
-        private void CountdownSpdBonusDecrease()
-        {
-            _spdBonusDecreaseTimer += Time.deltaTime;
-            if (_spdBonusDecreaseTimer >= _spdBonusDecreaseDelay)
-                _spdBonusDecreasing = true;
-        }
+        
     }
 }
