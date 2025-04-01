b090b9809d7df642e86f104d9b3565cbf4b9da32
Dartiro <beorgames@gmail.com>
2025-04-01 19:31:13 +0200

    Added GFX to Laser and Shotgun Turret, reordered turrets
    Added a laser beam to the laser turret and downloaded the pop ups pack

--- Idle Defense/Assets/Scripts/Turrets/LaserTurret.cs
+++ Idle Defense/Assets/Scripts/Turrets/LaserTurret.cs
@@ -11,6 +11,10 @@ namespace Assets.Scripts.Turrets
 
         private float _bonusDmgPerSec;
 
+        private bool _isShooting;
+
+        [SerializeField] private LineRenderer _laserLine;
+
         private void Awake()
         {
             _bonusDmgPerSec = 1f + _turretInfo.ProcentBonusDamagePerSec / 100f;
@@ -26,20 +30,51 @@ namespace Assets.Scripts.Turrets
                 _rampedDamageBonus = 0f;
             }
 
+            if (_isShooting)
+            {
+                if (_targetEnemy != null)
+                {
+                    _laserLine.enabled = true;
+
+                    _laserLine.SetPosition(0, _laserLine.transform.position); // always start from barrel
+                    _laserLine.SetPosition(1, _targetEnemy.transform.position); // always aim at live enemy
+                }
+                else
+                {
+                    _laserLine.enabled = false;
+                    _isShooting = false;
+                }
+            }
+
             RampDamageOverTime();
         }
+
+        protected override void AimTowardsTarget(float bonusMultiplier)
+        {
+            _laserLine.enabled = false;
+
+            base.AimTowardsTarget(bonusMultiplier);
+        }
+
         protected override void Shoot()
         {
             base.Shoot();
 
             _targetEnemy.GetComponent<Enemy>().TakeDamage(_damage + _rampedDamageBonus);
+
+            _isShooting = true;            
+
             _timeSinceLastShot = 0f;
         }
 
         private void RampDamageOverTime()
         {
             if (_targetEnemy == null)
+            {
+                _laserLine.enabled = false;
+                _isShooting = false;
                 return;
+            }
 
             _timeOnSameTarget += Time.deltaTime;
             _rampedDamageBonus = _turretInfo.Damage * _bonusDmgPerSec * _timeOnSameTarget;
