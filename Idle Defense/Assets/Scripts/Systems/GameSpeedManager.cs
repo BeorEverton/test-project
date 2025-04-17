ef424c45fa5613de3a26a4a8403d32d7c2cfcdbe
ReneBossen <renebossen99@gmail.com>
2025-04-17 12:47:14 +0200

    Moved GameSpeedLogic from button to GameSpeedManager

--- Idle Defense/Assets/Scripts/Systems/GameSpeedManager.cs
+++ Idle Defense/Assets/Scripts/Systems/GameSpeedManager.cs
@@ -1,33 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Systems
 {
     public class GameSpeedManager : MonoBehaviour
     {
+        [SerializeField] private List<GameSpeedButton> _gameSpeedButtons;
+
         private PlayerInput _playerInput;
+        private int _currentButtonIndex = 0;
 
         private void Start()
         {
             _playerInput = new();
             _playerInput.GameSpeed.Enable();
-            _playerInput.GameSpeed.x1.performed += ctx => SetGameSpeed(1);
-            _playerInput.GameSpeed.x2.performed += ctx => SetGameSpeed(2);
-            _playerInput.GameSpeed.x3.performed += ctx => SetGameSpeed(3);
-            _playerInput.GameSpeed.x5.performed += ctx => SetGameSpeed(5);
+
+            InitGameSpeedButtons();
+        }
+
+        private void InitGameSpeedButtons()
+        {
+            _gameSpeedButtons.ForEach(button =>
+            {
+                button.Button.gameObject.SetActive(false);
+                button.Button.onClick.AddListener(ActivateNextButton);
+            });
+
+            //Activate 1x button
+            _gameSpeedButtons[_currentButtonIndex].Button.gameObject.SetActive(true);
+            SetGameSpeed(_gameSpeedButtons[_currentButtonIndex].GameSpeed);
         }
 
         private void OnDestroy()
         {
-            _playerInput.GameSpeed.x1.performed -= ctx => SetGameSpeed(1);
-            _playerInput.GameSpeed.x2.performed -= ctx => SetGameSpeed(2);
-            _playerInput.GameSpeed.x3.performed -= ctx => SetGameSpeed(3);
-            _playerInput.GameSpeed.x5.performed -= ctx => SetGameSpeed(5);
             _playerInput.GameSpeed.Disable();
+        }
+
+        public void ActivateNextButton()
+        {
+            _gameSpeedButtons[_currentButtonIndex].Button.gameObject.SetActive(false);
+            _currentButtonIndex = (_currentButtonIndex + 1) % _gameSpeedButtons.Count;
+            _gameSpeedButtons[_currentButtonIndex].Button.gameObject.SetActive(true);
+
+            SetGameSpeed(_gameSpeedButtons[_currentButtonIndex].GameSpeed);
         }
 
         public void SetGameSpeed(int speed)
         {
             Time.timeScale = speed;
         }
+    }
+
+    [System.Serializable]
+    public class GameSpeedButton
+    {
+        public Button Button;
+        public int GameSpeed;
     }
 }
